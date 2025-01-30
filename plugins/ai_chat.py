cfbe451d805a86dbcc724991a21d1f63c86228d5
nomyowo <whitehattk@outlook.com>
2025-01-31 00:37:28 +0800

    250128 aiChat更新

--- plugins/ai_chat.py
+++ plugins/ai_chat.py
@@ -1,10 +1,11 @@
 def chatService(client, question):
     response = client.chat.completions.create(
-        model="deepseek-chat",
+        model="deepseek-reasoner",
         messages=[
-            {"role": "system", "content": "You are a helpful assistant"},
+            {"role": "system", "content": "假设你是一只狐狸，现在请用狐狸的身份去回答下面的问题"},
             {"role": "user", "content": question},
         ],
         stream=False
     )
-    return "\n"+response.choices[0].message.content
+    return "\n" + "思考过程：\n" + response.choices[0].message.model_extra['reasoning_content'] + "\n" + "\n回答：\n" + \
+        response.choices[0].message.content
